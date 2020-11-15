d316656ed40f8ff72a03ac12e6ff3421b229f631
gorebogd <gorebogd@yandex.ru>
2020-11-16 01:46:42 +0300

    1st review 10 project final fixes

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ImagePopup(props) {
     return (
-        <div className={`popup popup_type_image ${props.card && 'popup_opened'}`}>
+        <div className={`popup popup_type_image ${props.isOpen && 'popup_opened'}`}>
             <div className="popup__container_type_image">
                 <button type="button" className="popup__close-button" onClick={props.onClose}></button>
                 <figure className="popup__figure">
